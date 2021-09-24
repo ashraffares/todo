42b2d83581f1e1ba2e7bfe0286826d007d2a501a
ashraffares <ashraffares090@gmail.com>
2021-09-24 13:10:28 +0200

    add env

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import fetchTodoData from './Redux/Actions/TodoActions';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(fetchTodoData);
+  }, []);
+
+  // eslint-disable-next-line no-console
+  console.log(state);
   return (
     <div className="App">
       working
