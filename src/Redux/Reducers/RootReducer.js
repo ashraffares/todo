3245184770be411826be251fac7e1a3e29bbae5f
ashraffares <ashraffares090@gmail.com>
2021-09-26 17:21:46 +0200

    Pass all reducer to combine reducer

--- src/Redux/Reducers/RootReducer.js
+++ src/Redux/Reducers/RootReducer.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import TodoReducer from './TodoReducer';
 import PostTodoReducer from './PostTodoReducer';
 import DeleteTodoReducer from './DeleteTodoReducer';
+import UpdateTodoReducer from './UpdateReducer';
+import GetTodoReducer from './GetTodoReducer';
 
 const RootReducer = combineReducers({
   TodoReducer,
   PostTodoReducer,
   DeleteTodoReducer,
+  UpdateTodoReducer,
+  GetTodoReducer,
 });
 
 export default RootReducer;
--- src/container/todoList/todo.js
+++ src/container/todoList/todo.js
@@ -1,22 +1,16 @@
 /* eslint-disable no-console */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchTodoData, DeleteTodo } from '../../Redux/Actions/TodoActions';
+import { GetTodo } from '../../Redux/Actions/TodoActions';
 
 const Todo = () => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
-  useEffect(() => {
-    dispatch(DeleteTodo());
-  }, []);
 
   useEffect(() => {
-    dispatch(fetchTodoData());
+    dispatch(GetTodo(5));
   }, []);
 
-  if (state.TodoReducer.items.length > 0) {
-    console.log(state.TodoReducer.items);
-  }
   console.log('#################');
   console.log(state);
   console.log('###############');
