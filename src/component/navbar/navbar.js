1a8c707ede64f386245efa78568a212b6ea9ff30
ashraffares <ashraffares090@gmail.com>
2021-09-26 18:17:59 +0200

    Style navbar

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -1,16 +1,17 @@
 /* eslint-disable no-console */
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import styles from './navbar.module.css';
 
 const Navbar = () => {
   const location = useLocation();
   console.log(location);
   return (
-    <div>
+    <nav className={styles.navbar}>
       <i className="fas fa-bars" />
-      <h1>Todos</h1>
+      <h1 className={styles.header}>Todos</h1>
       <i className="fas fa-search" />
-    </div>
+    </nav>
   );
 };
 
