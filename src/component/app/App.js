3c311b2bff8fd05868e29be37542709402115422
ashraffares <ashraffares090@gmail.com>
2021-09-26 20:17:40 +0200

    Add style to form component

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Todos from '../../container/todoList/todos';
 import Navbar from '../navbar';
+import Form from '../form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Form />
       <Switch>
         <Route path="/" exact component={Todos} />
         <Redirect to="/" />
--- src/component/form/form.js
+++ src/component/form/form.js
@@ -1,17 +1,26 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import styles from './form.module.css';
 import { postTodo } from '../../Redux/Actions/TodoActions';
 
 const Form = () => {
   const dispatch = useDispatch();
   const [Title, SetTitle] = useState('');
   const [createdBy, SetcreatedBy] = useState('');
+  const obj = {
+    title: Title,
+    created_by: createdBy,
+    user_id: 1,
+  };
+
   return (
-    <form onSubmit={() => dispatch(postTodo({ title: Title, created_by: createdBy, user_id: 1 }))}>
-      <input type="text" placeholder="Todo Title" value={Title} onChange={(e) => SetTitle(e.target.value)} />
-      <input type="text" placeholder="Todo creator" value={createdBy} onChange={(e) => SetcreatedBy(e.target.value)} />
-      <button type="submit">Submit</button>
-    </form>
+    <div className={styles.wrapper}>
+      <form onSubmit={() => dispatch(postTodo(obj))} className={styles.form}>
+        <input type="text" placeholder="Todo Title" value={Title} onChange={(e) => SetTitle(e.target.value)} />
+        <input type="text" placeholder="Creater by" value={createdBy} onChange={(e) => SetcreatedBy(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
