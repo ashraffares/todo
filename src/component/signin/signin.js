a9bbbd49a3e676b13d71810ecd7701c6a979626b
ashraffares <ashraffares090@gmail.com>
2021-09-27 18:56:31 +0200

    Add Signin and signup fomrs

---
+++ src/component/signin/signin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignIn = () => (
+  <form>
+    <input type="text" placeholder="Email" />
+    <input type="text" placeholder="Password" />
+    <button type="submit">Submit</button>
+  </form>
+);
+
+export default SignIn;
---
+++ src/component/signup/signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Signup = () => (
+  <form>
+    <input type="text" placeholder="Email" />
+    <input type="text" placeholder="Password" />
+    <input type="text" placeholder="Password_confirmation" />
+    <button type="submit">Submit</button>
+  </form>
+);
+
+export default Signup;
