123248dc9b6af8bc20328be72253b94d908db55c
ashraffares <ashraffares090@gmail.com>
2021-09-27 10:13:19 +0200

    Add itemform compoennt

---
+++ src/component/itemform/itemform.js
@@ -0,0 +1,21 @@
+/* eslint-disable max-len */
+import React, { useState } from 'react';
+
+const ItemForm = () => {
+  const [Name, SetName] = useState();
+  const [Description, SetDescription] = useState();
+  const [Proirity, setProirity] = useState();
+  const [TodoId, setTodoid] = useState();
+
+  return (
+    <form>
+      <input placeholder="Name" value={Name} onClick={(e) => SetName(e.target.value)} />
+      <input placeholder="Description" value={Description} onClick={(e) => SetDescription(e.target.value)} />
+      <input placeholder="Proirity" value={Proirity} onClick={(e) => setProirity(e.target.value)} />
+      <input placeholder="TodoId" value={TodoId} onClick={(e) => setTodoid(e.target.value)} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default ItemForm;
